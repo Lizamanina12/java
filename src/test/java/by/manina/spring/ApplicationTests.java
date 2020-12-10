b9a005e318a211d29b44f0adc10562f8111c823f
Liza manina <manina2048@mail.ru>
2020-12-10 21:41:39 +0300

    Test2

--- src/test/java/by/manina/spring/ApplicationTests.java
+++ src/test/java/by/manina/spring/ApplicationTests.java
@@ -1,7 +1,13 @@
 package by.manina.spring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class ApplicationTests {
@@ -9,5 +15,16 @@ class ApplicationTests {
     @Test
     void contextLoads() {
     }
+    @Autowired
+    private MockMvc mockMvc;
 
+    @org.junit.Test
+    public void LoadUser() throws Exception {
+        mockMvc.perform(post("/test")).andDo(print()).andExpect(status().is2xxSuccessful());
+    }
+
+    @org.junit.Test
+    public void LoadUserEx() throws Exception {
+        mockMvc.perform(post("/register")).andDo(print()).andExpect(status().is4xxClientError());
+    }
 }
